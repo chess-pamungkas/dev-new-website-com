03f11fd2b6026fa393224ff2cc09d3236f650abf
Yauheniya Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-08-17 16:02:04 +0300

    feat: crypto page wip

--- src/components/top-market/index.js
+++ src/components/top-market/index.js
@@ -1,13 +1,46 @@
 import React from "react";
 import cn from "classnames";
+import ButtonLink from "../shared/button-link";
 
 const TopMarket = ({
   className,
+  children,
+  title,
+  image,
+  btn1Title,
+  link1,
+  btn2Title,
+  link2,
 }) => {
-
-  return <section className={cn("top-market", className)}>
-
-  </section>;
+  return (
+    <section className={cn("top-market", className)}>
+      <div className={"top-market__wrapper"}>
+        <div className="top-market__block">
+          <h2 className="top-market__title">{title}</h2>
+          <div className="top-market__description">
+            <p className="top-market__text">{children}</p>
+          </div>
+          <div className="top-market__btn-wrapper">
+            <ButtonLink
+              link={link1}
+              className={cn("top-market__btn", "top-market__btn--black")}
+            >
+              {btn1Title}
+            </ButtonLink>
+            <ButtonLink
+              link={link2}
+              className={cn("top-market__btn", "top-market__btn--white")}
+            >
+              {btn2Title}
+            </ButtonLink>
+          </div>
+        </div>
+        <div className={cn("top-market__block", "top-market__block--flexed")}>
+          <img src={image} alt="" className="top-market__img" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default TopMarket;
--- src/pages/crypto.js
+++ src/pages/crypto.js
@@ -1,21 +1,32 @@
 import React from "react";
 import "../assets/styles/index.scss";
 import Header from "../components/header";
-import MainPromotion from "../components/main-promotion";
-import {ClientResolverProvider} from "../context/client-resolver-context/entity-resolver-context";
-import Popup from "../components/popup"
-
+import { ClientResolverProvider } from "../context/client-resolver-context/entity-resolver-context";
+import Popup from "../components/popup";
+import TopMarket from "../components/top-market";
+import { REGISTRATION_LINK } from "../helpers/constants";
+import image from "../assets/images/top-markets/cripto.svg";
+import { CRYPTO_TEXT } from "../helpers/top-market-texts";
 
 const CryptoPage = () => {
   return (
     <>
-    <ClientResolverProvider>
-      <Header />
-      <main>
-        <Popup />
-        <MainPromotion />
-      </main>
-    </ClientResolverProvider>
+      <ClientResolverProvider>
+        <Header />
+        <main>
+          <Popup />
+          <TopMarket
+            title="You can now trade more than xxx crypto pairs!"
+            image={image}
+            btn1Title="Try our demo account"
+            link1={REGISTRATION_LINK}
+            btn2Title="Trade now crypto cfc"
+            link2={REGISTRATION_LINK}
+          >
+            {CRYPTO_TEXT}
+          </TopMarket>
+        </main>
+      </ClientResolverProvider>
     </>
   );
 };
