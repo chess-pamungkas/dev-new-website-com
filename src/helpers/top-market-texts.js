a10e5a03caaed75d95a4aa51fa5924dbb0c116ac
Yauheniya.Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-08-18 11:30:19 +0300

    feat: added the page

--- src/helpers/top-market-texts.js
+++ src/helpers/top-market-texts.js
@@ -107,3 +107,21 @@ export const ENERGIES_TEXT = (
         <span className="highlighted-in-black">.</span>
     </>
 );
+
+export const ALL_MARKET_TEXT = (
+    <>
+        <span className="highlighted-in-black">Take&nbsp;</span>
+        <span className="highlighted-in-white">advantage&nbsp;</span>
+        <span className="highlighted-in-black">of&nbsp;</span>
+        <span className="highlighted-in-white">
+      professional deep liquidity&nbsp;
+    </span>
+        <span className="highlighted-in-black">and&nbsp;</span>
+        <span className="highlighted-in-white">leverage&nbsp;</span>
+        <span className="highlighted-in-black">
+      on the hottest asset of the&nbsp;
+    </span>
+        <span className="highlighted-in-white">last decade</span>
+        <span className="highlighted-in-black">.</span>
+    </>
+);
