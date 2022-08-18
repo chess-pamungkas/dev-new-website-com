42e602484504f73a52f737b68f36855d61be6c0f
Aliaksandr Baryhin <aliaksandr.baryhin@itechart-group.com>
2022-08-18 13:08:01 +0300

    Changed detect browser language service

--- src/helpers/services/detect-browser-settings.js
+++ src/helpers/services/detect-browser-settings.js
@@ -1,6 +1,8 @@
-export const detectBrowserLanguage = () =>
-  {
-    var language = (window.navigator.languages && window.navigator.languages[0]) ||
+const LANG_CODE_CHARS_COUNT = 2;
+
+export const detectBrowserLanguage = () => {
+  const language =
+    (window.navigator.languages && window.navigator.languages[0]) ||
     window.navigator.language;
-    return language.substring(0, 2);
-  }
+  return language.substring(0, LANG_CODE_CHARS_COUNT);
+};
