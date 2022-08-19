c81eca518d6e235ee75f2427358ee54f463e2d3e
Aliaksandr Baryhin <aliaksandr.baryhin@itechart-group.com>
2022-08-19 14:37:14 +0300

    Fixed menu items for cysec

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import cn from "classnames";
 import { LogoTextMain, Logo } from "../shared/icons";
 import { useWindowSize } from "../../helpers/hooks/use-window-size";
 import { WINDOW_SIZE_XL } from "../../helpers/constants";
-import { MENU_ITEMS } from "../../helpers/menu.config";
 import { stringTransformToKebabCase } from "../../helpers/services/string-service";
 import NavbarItem from "./components/navbar-item";
 import LangSelect from "./components/lang-select";
 import BurgerMenu from "./components/burger-menu";
 import ButtonLink from "../shared/button-link";
+import { CYSEC_MENU_ITEMS, FSA_MENU_ITEMS } from "../../helpers/menu.config";
+import ClientResolverContext from "../../context/client-resolver-context";
+import entities from "../../enums/entities";
 
 const Header = ({ className }) => {
   const { width, isTablet } = useWindowSize();
+  const [menu, setMenu] = useState([]);
+  const { currentEntity } = useContext(ClientResolverContext);
 
   const isNarrow = width < WINDOW_SIZE_XL;
+
+  useEffect(() => {
+    setMenu(
+      currentEntity === entities.CYSEC ? CYSEC_MENU_ITEMS : FSA_MENU_ITEMS
+    );
+  }, [currentEntity]);
 
   return (
     <header className={cn("header", { "header--small": isNarrow }, className)}>
@@ -22,7 +32,7 @@ const Header = ({ className }) => {
 
         {!isTablet && (
           <ul className="header__navigation">
-            {MENU_ITEMS.map(({ title, subItems, isNested = false }) => (
+            {menu.map(({ title, subItems, isNested = false }) => (
               <NavbarItem
                 key={`header-menu-${stringTransformToKebabCase(title)}`}
                 title={title}
