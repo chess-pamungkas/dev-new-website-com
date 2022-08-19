a450311e049086080d5aa4faad6b9dd4c67abe33
Yauheniya.Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-08-19 12:56:13 +0300

    refactor: refactored icons

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -17,58 +17,58 @@ import {
 
 export const CYSEC_ADVANTAGES = [
   {
-    icon: <AdvantageIcon1 className="advantage-block__icon" />,
+    icon: AdvantageIcon1,
     text: ADVANTAGE_TEXTS.block1,
   },
   {
-    icon: <AdvantageIcon2 className="advantage-block__icon" />,
+    icon: AdvantageIcon2,
     text: ADVANTAGE_TEXTS.block2,
   },
   {
-    icon: <AdvantageIcon3 className="advantage-block__icon" />,
+    icon: AdvantageIcon3,
     text: ADVANTAGE_TEXTS.block3,
   },
   {
-    icon: <AdvantageIcon4 className="advantage-block__icon" />,
+    icon: AdvantageIcon4,
     text: ADVANTAGE_TEXTS.block4,
   },
   {
-    icon: <AdvantageIcon6 className="advantage-block__icon" />,
+    icon: AdvantageIcon6,
     text: ADVANTAGE_TEXTS.block6,
   },
   {
-    icon: <AdvantageIcon7 className="advantage-block__icon" />,
+    icon: AdvantageIcon7,
     text: ADVANTAGE_TEXTS.block7,
   },
 ];
 
 export const FSA_ADVANTAGES = [
   {
-    icon: <AdvantageIcon1 className="advantage-block__icon" />,
+    icon: AdvantageIcon1,
     text: ADVANTAGE_TEXTS.block1,
   },
   {
-    icon: <AdvantageIcon2 className="advantage-block__icon" />,
+    icon: AdvantageIcon2,
     text: ADVANTAGE_TEXTS.block2,
   },
   {
-    icon: <AdvantageIcon3 className="advantage-block__icon" />,
+    icon: AdvantageIcon3,
     text: ADVANTAGE_TEXTS.block3,
   },
   {
-    icon: <AdvantageIcon4 className="advantage-block__icon" />,
+    icon: AdvantageIcon4,
     text: ADVANTAGE_TEXTS.block4,
   },
   {
-    icon: <AdvantageIcon5 className="advantage-block__icon" />,
+    icon: AdvantageIcon5,
     text: ADVANTAGE_TEXTS.block5,
   },
   {
-    icon: <AdvantageIcon6 className="advantage-block__icon" />,
+    icon: AdvantageIcon6,
     text: ADVANTAGE_TEXTS.block6,
   },
   {
-    icon: <AdvantageIcon7 className="advantage-block__icon" />,
+    icon: AdvantageIcon7,
     text: ADVANTAGE_TEXTS.block7,
   },
 ];
