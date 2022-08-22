826a45238861ee942aeb3ade0730fd91c0cacb12
alexyush93 <alexyush93@gmail.com>
2022-08-22 10:19:48 +0300

    feat: trading tools animation

--- src/components/trading-tools/components/device-block/index.js
+++ src/components/trading-tools/components/device-block/index.js
@@ -1,16 +1,51 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import cn from "classnames";
 import phoneIcon from "../../../../assets/images/icons/phone.svg";
 import tabletIcon from "../../../../assets/images/icons/tablet.svg";
 import laptopIcon from "../../../../assets/images/icons/laptop.svg";
 import devicesIcon from "../../../../assets/images/icons/devices-sm.svg";
 
-const DeviceBlock = ({ className }) => {
+const DeviceBlock = ({ className, isAnimationStarted }) => {
+  const calculateLeftPosition = (ref) => {
+    const clientRect = ref.current?.getBoundingClientRect();
+    return -(clientRect?.left + clientRect?.width) + 'px';
+  }
+
+  const phoneIconRef = useRef();
+  const tabletIconRef = useRef();
+  const laptopIconRef = useRef();
+
+  const [phoneLeftPosition, setPhoneLeftPosition] = useState('inherit');
+  const [tabletLeftPosition, setTabletLeftPosition] = useState('inherit');
+  const [laptopLeftPosition, setLaptopLeftPosition] = useState('inherit');
+
+  useEffect(() => {
+    setPhoneLeftPosition(calculateLeftPosition(phoneIconRef));
+  }, [phoneIconRef]);
+
+  useEffect(() => {
+    setTabletLeftPosition(calculateLeftPosition(tabletIconRef));
+  }, [tabletIconRef]);
+
+  useEffect(() => {
+    setLaptopLeftPosition(calculateLeftPosition(laptopIconRef));
+  }, [laptopIconRef]);
+
+  useEffect(() => {
+    if (isAnimationStarted) {
+      setLaptopLeftPosition(null);
+      setTabletLeftPosition(null);
+      setPhoneLeftPosition(null);
+    }
+  }, [isAnimationStarted]);
+
   return (
     <div className={cn("device-block", className)}>
       <img
+        ref={phoneIconRef}
         src={phoneIcon}
         alt="Phone"
+        style={{ left: phoneLeftPosition }}
         className={cn(
           "device-block__img",
           "device-block__img--phone",
@@ -18,8 +53,10 @@ const DeviceBlock = ({ className }) => {
         )}
       />
       <img
+        ref={tabletIconRef}
         src={tabletIcon}
         alt="Tablet"
+        style={{ left: tabletLeftPosition }}
         className={cn(
           "device-block__img",
           "device-block__img--tablet",
@@ -27,8 +64,10 @@ const DeviceBlock = ({ className }) => {
         )}
       />
       <img
+        ref={laptopIconRef}
         src={laptopIcon}
         alt="Laptop"
+        style={{ left: laptopLeftPosition }}
         className={cn(
           "device-block__img",
           "device-block__img--laptop",
--- src/components/trading-tools/index.js
+++ src/components/trading-tools/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import cn from "classnames";
 import { PLATFORMS } from "../../helpers/config";
 import PlatformBlock from "./components/platform-block";
@@ -6,21 +6,48 @@ import { TRADING_TOOLS_TITLE } from "../../helpers/promo-texts";
 import ButtonLink from "../shared/button-link";
 import DeviceBlock from "./components/device-block";
 import {REGISTRATION_LINK} from "../../helpers/constants";
+import {useTrail, animated} from "react-spring";
 
 const TradingTools = ({ className }) => {
+  const [isAnimationStarted, setIsAnimationStarted] = useState(false);
+
+  const platformIconTrail = useTrail(Object.values(PLATFORMS).length, {
+    from: {
+      position: "relative",
+      bottom: "-40px",
+      opacity: 0,
+      config: {
+        duration: 80
+      }
+    },
+    to: {
+      bottom: "0",
+      opacity: 1,
+    },
+    delay: 2500,
+  });
+
+  setTimeout(() => {
+    setIsAnimationStarted(true);
+  }, 1000)
+
   return (
     <section className={cn("trading-tools", className)}>
       <div className="trading-tools__wrapper">
         <div className="trading-tools__icon-wrapper">
-          {Object.values(PLATFORMS).map((platform) => (
-            <PlatformBlock
-              key={`platform-${platform.title}`}
-              icon={platform.icon}
-              title={platform.title}
-            />
-          ))}
+          {platformIconTrail.map((styles, i) => {
+            return (
+              <animated.span style={styles}>
+                <PlatformBlock
+                  key={`platform-${Object.values(PLATFORMS)[i].title}`}
+                  icon={Object.values(PLATFORMS)[i].icon}
+                  title={Object.values(PLATFORMS)[i].title}
+                />
+              </animated.span>
+            )
+          })}
         </div>
-        <DeviceBlock className="trading-tools__img-wrapper" />
+        <DeviceBlock className="trading-tools__img-wrapper device-block--animated" isAnimationStarted={isAnimationStarted} />
         <h2 className="trading-tools__title">{TRADING_TOOLS_TITLE}</h2>
         <ButtonLink link={REGISTRATION_LINK} className="trading-tools__btn">
           Create your account
