8867efa7eb773b675589db5cc6a434193ddd3d8e
Aliaksei Yushkevich <aliaksei.yushkevich@itechart-group.com>
2022-08-23 11:28:57 +0300

    fix: gatsby ga plugin config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   plugins: ["gatsby-plugin-sass", {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
-      "trackingId": "UA-000000-2"
+      "trackingId": process.env.GATSBY_GA
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
