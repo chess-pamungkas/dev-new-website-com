dc0f01165a37deaef60a9b171c1ce9018d87b20a
Yauheniya Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-08-24 15:28:29 +0300

    refactor: refactoring

--- src/helpers/hooks/use-window-size.js
+++ src/helpers/hooks/use-window-size.js
@@ -1,28 +1,32 @@
-import { useEffect, useState, useCallback } from 'react';
-import { WINDOW_SIZE_MD, WINDOW_SIZE_LG } from '../constants';
+import { useEffect, useState, useCallback } from "react";
+import { WINDOW_SIZE_MD, WINDOW_SIZE_LG, WINDOW_SIZE_XL } from "../constants";
 
 export const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
-    width: typeof window !== 'undefined' && window.innerWidth,
-    height: typeof window !== 'undefined' && window.innerHeight
+    width: typeof window !== "undefined" && window.innerWidth,
+    height: typeof window !== "undefined" && window.innerHeight,
   });
 
   const handleResize = useCallback(() => {
     setWindowSize({
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
     });
   }, []);
 
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [handleResize]);
 
   return {
     ...windowSize,
     isMobile: windowSize.width < WINDOW_SIZE_MD,
-    isTablet: windowSize.width < WINDOW_SIZE_LG
+    isTablet:
+      windowSize.width >= WINDOW_SIZE_MD && windowSize.width < WINDOW_SIZE_LG,
+    isLG:
+      windowSize.width >= WINDOW_SIZE_LG && windowSize.width < WINDOW_SIZE_XL,
+    isXL: windowSize.width >= WINDOW_SIZE_XL,
   };
 };
