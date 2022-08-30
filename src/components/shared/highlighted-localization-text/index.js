c592536fff3bfff3752d7b13f376902f15c10edf
pavel asadchy <asadchy.pavel@gmail.com>
2022-08-30 23:46:28 +0300

    feat: promotion localization

--- src/components/shared/highlighted-localization-text/index.js
+++ src/components/shared/highlighted-localization-text/index.js
@@ -1,28 +1,36 @@
 import React from "react";
 import cn from "classnames";
 import { useTranslation } from "gatsby-plugin-react-i18next";
-import { stringTransformToKebabCase } from "../../../helpers/services/string-service";
 
 const HighlightedLocalizationText = ({
   localizationText = '',
   wordsToHighlight = '',
-  accentClassName = ''
+  primaryClassName = '',
+  accentClassName = '',
+  children
 }) => {
   const { t } = useTranslation();
 
   return (
     <>
-      {t(localizationText).split(' ').map((word, i, array) => (
-        <span
-          key={`highlighted-localization-text-${stringTransformToKebabCase(word)}`}
-          className={cn({
-            [`${accentClassName}`]: [...t(wordsToHighlight)].includes(String(i + 1))
-          })}
-        >
-          {word}
-          {i !== array.length - 1 && ' '}
-        </span>
-      ))}
+      {t(localizationText).split(' ').map((word, i, array) => {
+        const isWordHighlighted = t(wordsToHighlight).split(',').includes(String(i + 1));
+
+        return (
+          <span
+            key={`highlighted-localization-text-${i}`}
+            className={cn(
+              {[`${accentClassName}`]: isWordHighlighted},
+              {[`${primaryClassName}`]: !isWordHighlighted && primaryClassName}
+            )}
+          >
+            {word}
+            {i !== array.length - 1 && ' '}
+          </span>
+        );
+      })}
+
+      {children}
     </>
   );
 };
--- src/helpers/promo-texts.js
+++ src/helpers/promo-texts.js
@@ -1,70 +1,70 @@
 import React from "react";
 
-export const PROMO_TEXT_1 = (
-  <>
-    <span className="highlighted-in-red">
-      Start building your trading portfolio&nbsp;
-    </span>
-    <span className="highlighted-in-white">
-      with as little as $XX USD&nbsp;
-    </span>
-    <span className="highlighted-in-red">or equivalent with&nbsp;</span>
-    <span className="highlighted-in-white">X base currencies&nbsp;</span>
-    <span className="highlighted-in-red">available.</span>
-  </>
-);
+// export const PROMO_TEXT_1 = (
+//   <>
+//     <span className="highlighted-in-red">
+//       Start building your trading portfolio&nbsp;
+//     </span>
+//     <span className="highlighted-in-white">
+//       with as little as $XX USD&nbsp;
+//     </span>
+//     <span className="highlighted-in-red">or equivalent with&nbsp;</span>
+//     <span className="highlighted-in-white">X base currencies&nbsp;</span>
+//     <span className="highlighted-in-red">available.</span>
+//   </>
+// );
 
-export const PROMO_TEXT_2 = (
-  <>
-    <span className="highlighted-in-black">Trade&nbsp;</span>
-    <span className="highlighted-in-red">EURUSD</span>
-    <span className="highlighted-in-black">,&nbsp;</span>
-    <span className="highlighted-in-red">XAUUSD&nbsp;</span>
-    <span className="highlighted-in-black">and&nbsp;</span>
-    <span className="highlighted-in-red">many other&nbsp;</span>
-    <span className="highlighted-in-black">assets with one of the&nbsp;</span>
-    <span className="highlighted-in-red">
-      lowest average spreads in the industry
-    </span>
-    <span className="highlighted-in-black">, starting at&nbsp;</span>
-    <span className="highlighted-in-red">0.0 pips</span>
-    <span className="highlighted-in-black">.</span>
-  </>
-);
+// export const PROMO_TEXT_2 = (
+//   <>
+//     <span className="highlighted-in-black">Trade&nbsp;</span>
+//     <span className="highlighted-in-red">EURUSD</span>
+//     <span className="highlighted-in-black">,&nbsp;</span>
+//     <span className="highlighted-in-red">XAUUSD&nbsp;</span>
+//     <span className="highlighted-in-black">and&nbsp;</span>
+//     <span className="highlighted-in-red">many other&nbsp;</span>
+//     <span className="highlighted-in-black">assets with one of the&nbsp;</span>
+//     <span className="highlighted-in-red">
+//       lowest average spreads in the industry
+//     </span>
+//     <span className="highlighted-in-black">, starting at&nbsp;</span>
+//     <span className="highlighted-in-red">0.0 pips</span>
+//     <span className="highlighted-in-black">.</span>
+//   </>
+// );
 
-export const PROMO_TEXT_3 = (
-  <>
-    <span className="highlighted-in-black">
-      Your money, your way, enjoy&nbsp;
-    </span>
-    <span className="highlighted-in-white">instant withdrawals&nbsp;</span>
-    <span className="highlighted-in-black">
-      with multiple withdrawal methods and multiple&nbsp;
-    </span>
-    <span className="highlighted-in-white">
-      trusted funding channels with no fees
-    </span>
-    <span className="highlighted-in-black">.</span>
-  </>
-);
+// export const PROMO_TEXT_3 = (
+//   <>
+//     <span className="highlighted-in-black">
+//       Your money, your way, enjoy&nbsp;
+//     </span>
+//     <span className="highlighted-in-white">instant withdrawals&nbsp;</span>
+//     <span className="highlighted-in-black">
+//       with multiple withdrawal methods and multiple&nbsp;
+//     </span>
+//     <span className="highlighted-in-white">
+//       trusted funding channels with no fees
+//     </span>
+//     <span className="highlighted-in-black">.</span>
+//   </>
+// );
 
-export const PROMO_TEXT_4 = (
-  <>
-    <span className="highlighted-in-white">
-      Studying great traders is good but exactly copying them is even better!
-      Look like a genius of the&nbsp;
-    </span>
-    <span className="highlighted-in-red">investment&nbsp;</span>
-    <span className="highlighted-in-white">with a few clicks.</span>
-    <span className="promotion__note">
-      <span className="highlighted-in-white">Find a trader you&nbsp;</span>
-      <span className="highlighted-in-red">believe in</span>
-      <span className="highlighted-in-white">
-        , start copying them and get the same results.
-      </span>
-    </span>
-  </>
-);
+// export const PROMO_TEXT_4 = (
+//   <>
+//     <span className="highlighted-in-white">
+//       Studying great traders is good but exactly copying them is even better!
+//       Look like a genius of the&nbsp;
+//     </span>
+//     <span className="highlighted-in-red">investment&nbsp;</span>
+//     <span className="highlighted-in-white">with a few clicks.</span>
+//     <span className="promotion__note">
+//       <span className="highlighted-in-white">Find a trader you&nbsp;</span>
+//       <span className="highlighted-in-red">believe in</span>
+//       <span className="highlighted-in-white">
+//         , start copying them and get the same results.
+//       </span>
+//     </span>
+//   </>
+// );
 
 export const TRADING_TOOLS_TITLE = (
   <>
