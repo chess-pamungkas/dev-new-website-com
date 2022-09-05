f2960c7694245607e624fdae09997a6a898dc144
Aliaksandr Baryhin <aliaksandr.baryhin@itechart-group.com>
2022-09-05 11:51:01 +0300

    Fixed cookie popup layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,8 +24,6 @@ import {
   BACKGROUND_ANIMATION_DURATION,
 } from "../helpers/animation.config";
 import { useWindowSize } from "../helpers/hooks/use-window-size";
-import { CookiesPopup } from "../components/cookies-popup";
-import { GDPRPopup } from "../components/gdpr-popup";
 import cn from "classnames";
 import { scrollTo } from "../helpers/scroll-to";
 import Seo from "../components/shared/seo";
@@ -221,8 +219,6 @@ const IndexPage = () => {
 
   return (
     <Layout headerRef={headerRef}>
-      <CookiesPopup />
-      <GDPRPopup />
       <Seo title={t("page-main-title")} />
       <MainPromotion />
       <TradingTicker />
