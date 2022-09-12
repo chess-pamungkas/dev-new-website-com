b3ff9fd961b7b2ee68738360bcb8eba365c05682
Aliaksandr Baryhin <aliaksandr.baryhin@itechart-group.com>
2022-09-12 15:28:20 +0300

    Added responsive layout for all markets page

--- src/components/all-markets/index.js
+++ src/components/all-markets/index.js
@@ -10,8 +10,12 @@ const AllMarkets = ({ className }) => {
     <section className={cn("all-markets", className)}>
       <div className="all-markets__wrapper">
         <div className="all-markets__block">
-          <h2 className="all-markets__title">{t("all-markets_all-markets-title")}</h2>
-          <div className="all-markets__text">{t("all-markets_all-markets-text")}</div>
+          <h2 className="all-markets__title">
+            {t("all-markets_all-markets-title")}
+          </h2>
+          <div className="all-markets__text">
+            {t("all-markets_all-markets-text")}
+          </div>
         </div>
         <div className={cn("all-markets__block", "all-markets__block--flexed")}>
           <img src={image} alt="" className="all-markets__img" />
