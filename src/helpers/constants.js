595fd99f69c8a0c1e0fd3e2e5dfe3457ea4cb1de
Aliaksei Yushkevich <aliaksei.yushkevich@itechart-group.com>
2022-09-30 13:06:30 +0300

    fix: merge conflicts

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -46,6 +46,7 @@ export const ALL_MARKETS_PAGE_LINK = "/all-markets";
 export const MT4_PAGE_LINK = "/mt4";
 export const MT5_PAGE_LINK = "/mt5";
 export const WITHDRAWAL_PAGE_LINK = "/withdrawal";
+export const SPREADS_AND_FEES_PAGE_LINK = "/spreads-and-fees";
 export const PROFESSIONAL_QUALIFICATION_PAGE_LINK = "/professional-qualification";
 
 export const FAQ_LINK = "/";
