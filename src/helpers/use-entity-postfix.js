00d1819590bb349b3ed65658d4118bf71fd72f12
Yauheniya Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-10-06 15:00:17 +0300

    refactor: added hook for entity postfix

---
+++ src/helpers/use-entity-postfix.js
@@ -0,0 +1,21 @@
+import { useContext, useEffect, useState } from "react";
+import ClientResolverContext from "../context/client-resolver-context";
+import entities from "../enums/entities";
+import { FSA_POSTFIX } from "./constants";
+
+export const useEntityPostfix = () => {
+  const { currentEntity } = useContext(ClientResolverContext);
+
+  const [sitePostfix, setSitePostfix] = useState("");
+  const [isCySEC, setIsCySEC] = useState(null);
+
+  useEffect(() => {
+    setIsCySEC(currentEntity === entities.CYSEC);
+    setSitePostfix(currentEntity === entities.FSA ? FSA_POSTFIX : "");
+  }, [currentEntity]);
+
+  return {
+    sitePostfix,
+    isCySEC,
+  };
+};
