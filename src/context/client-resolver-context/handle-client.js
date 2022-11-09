6aa46725c5e5e60dad9afc4bd91734cd539da22d
Yauheniya Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-11-09 14:17:15 +0300

    refactor: added console warn

--- src/context/client-resolver-context/handle-client.js
+++ src/context/client-resolver-context/handle-client.js
@@ -2,7 +2,14 @@ const handleClient = (clientConfig, entityToRedirect, setIsPopupShown) => {
   if (clientConfig.banned || clientConfig.recommendedRedirect) {
     setIsPopupShown(true);
   } else if (clientConfig.forceRedirect) {
-    window.location.replace(entityToRedirect + window.location.pathname);
+    // TODO remove this after the bug is caught
+    console.warn("clientConfig: ", clientConfig);
+    console.warn(`entityToRedirect: ${entityToRedirect}`);
+    setTimeout(
+      () =>
+        window.location.replace(entityToRedirect + window.location.pathname),
+      10000
+    );
   }
 };
 
