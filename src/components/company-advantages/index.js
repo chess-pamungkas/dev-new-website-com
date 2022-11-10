9f3628ca295d5682a64fda5804502b306d20ed61
Yauheniya Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-11-10 15:58:51 +0300

    feat: added rtl for the company page

--- src/components/company-advantages/index.js
+++ src/components/company-advantages/index.js
@@ -3,10 +3,19 @@ import cn from "classnames";
 import CompanyAdvantageBlock from "./components/company-advantage-block";
 import { stringTransformToKebabCase } from "../../helpers/services/string-service";
 import { COMPANY_ADVANTAGES } from "../../helpers/config";
+import { useRtlDirection } from "../../helpers/hooks/use-rtl-direction";
+import { DIR_LTR, DIR_RTL } from "../../helpers/constants";
 
 const CompanyAdvantages = ({ className }) => {
+  const isRTL = useRtlDirection();
+
   return (
-    <section className={cn("company-advantages", className)}>
+    <section
+      className={cn("company-advantages", className, {
+        "company-advantages--rtl": isRTL,
+      })}
+      dir={isRTL ? DIR_RTL : DIR_LTR}
+    >
       <div className="company-advantages__wrapper">
         {COMPANY_ADVANTAGES.map((block) => (
           <CompanyAdvantageBlock
