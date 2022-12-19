0555b6dc58479fc449a5b4172a7648ef61e412a9
Yauheniya Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-12-19 20:14:45 +0300

    fix: fixed png

--- src/components/trading-tools/components/alpha-generation-tabs/index.js
+++ src/components/trading-tools/components/alpha-generation-tabs/index.js
@@ -8,7 +8,7 @@ import {
 import Tabs from "../../../shared/tabs";
 import alpha1 from "../../../../assets/images/trading-tools/alpha1.png";
 import alpha21 from "../../../../assets/images/trading-tools/alpha21.png";
-import alpha22 from "../../../../assets/images/trading-tools/alpha22.png";
+import alpha22 from "../../../../assets/images/trading-tools/alpha22.PNG";
 import alpha3 from "../../../../assets/images/trading-tools/alpha3.png";
 import TradingToolsTabContent from "../trading-tools-tab-content";
 
