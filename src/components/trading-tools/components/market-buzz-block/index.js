5a41851c6d2f23120944a5efa898c22174c4929f
Yauheniya Asmalouskaya <evgeniya.osmolovskaya@gmail.com>
2022-12-19 19:11:30 +0300

    style: added rtl for the trading tools page

--- src/components/trading-tools/components/market-buzz-block/index.js
+++ src/components/trading-tools/components/market-buzz-block/index.js
@@ -2,14 +2,24 @@ import React from "react";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import cn from "classnames";
 import { stringTransformToKebabCase } from "../../../../helpers/services/string-service";
+import { useRtlDirection } from "../../../../helpers/hooks/use-rtl-direction";
 
 const MarketBuzzBlock = ({ img, title, description }) => {
   const { t } = useTranslation();
+  const isRTL = useRtlDirection();
 
   return (
-    <div className={cn("mb-block")}>
+    <div
+      className={cn("mb-block", {
+        "mb-block--rtl": isRTL,
+      })}
+    >
       <div className="mb-block__img-wrapper">
-        <img className="mb-block__img" src={img} alt={stringTransformToKebabCase(title)} />
+        <img
+          className="mb-block__img"
+          src={img}
+          alt={stringTransformToKebabCase(title)}
+        />
       </div>
       <div className="mb-block__title-wrapper">
         <h2 className="mb-block__title">{t(title)}</h2>
