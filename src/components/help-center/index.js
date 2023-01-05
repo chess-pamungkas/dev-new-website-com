d94872fe9463e3a7bb2b913c2c8add999dc67b1d
Yiangos Stroppos <stroppos.yiangos@gmail.com>
2023-01-05 14:59:35 +0200

    cookie popup fix

--- src/components/help-center/index.js
+++ src/components/help-center/index.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import cn from "classnames";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import {
-  FAQ_ALL,
-  FAQ_BEGINNERS,
-  FAQ_MARKET,
-  FAQ_QUICK_ANSWER,
+	FAQ_ALL,
+	FAQ_BEGINNERS,
+	FAQ_MARKET,
+	FAQ_QUICK_ANSWER,
 } from "../../helpers/faq";
 import Faq from "../faq";
 import { stringTransformToKebabCase } from "../../helpers/services/string-service";
@@ -15,103 +15,103 @@ import { useRtlDirection } from "../../helpers/hooks/use-rtl-direction";
 import { DIR_LTR, DIR_RTL } from "../../helpers/constants";
 
 const HelpCenter = ({ className }) => {
-  const { t } = useTranslation();
-  const [searchResults, setSearchResults] = useState([]);
-  const isRTL = useRtlDirection();
+	const { t } = useTranslation();
+	const [searchResults, setSearchResults] = useState([]);
+	const isRTL = useRtlDirection();
 
-  const HelpCenterBlock = ({
-    title,
-    subtitle,
-    faq,
-    titleClassName,
-    classNames,
-  }) => (
-    <div className="help-center__block">
-      {title && (
-        <h4 className={cn("help-center__block-title", titleClassName)}>
-          {title}
-        </h4>
-      )}
-      {subtitle && <p className="help-center__block-subtitle">{subtitle}</p>}
-      {faq.map((item, i) => (
-        <Faq
-          key={`faq-${stringTransformToKebabCase(
-            item.title || title || `faq-search-${i}`
-          )}`}
-          title={
-            item.title && (
-              <>
-                <img
-                  src={item.icon}
-                  alt=""
-                  className="help-center__title-icon"
-                />
-                <span>{t(item.title)}</span>
-              </>
-            )
-          }
-          faq={item.content}
-          className={cn("faq--help-center", classNames)}
-          isFaqBtnHidden
-        />
-      ))}
-    </div>
-  );
+	const HelpCenterBlock = ({
+		title,
+		subtitle,
+		faq,
+		titleClassName,
+		classNames,
+	}) => (
+		<div className="help-center__block">
+			{title && (
+				<h4 className={cn("help-center__block-title", titleClassName)}>
+					{title}
+				</h4>
+			)}
+			{subtitle && <p className="help-center__block-subtitle">{subtitle}</p>}
+			{faq.map((item, i) => (
+				<Faq
+					key={`faq-${stringTransformToKebabCase(
+						item.title || title || `faq-search-${i}`
+					)}`}
+					title={
+						item.title && (
+							<>
+								<img
+									src={item.icon}
+									alt=""
+									className="help-center__title-icon"
+								/>
+								<span>{t(item.title)}</span>
+							</>
+						)
+					}
+					faq={item.content}
+					className={cn("faq--help-center", classNames)}
+					isFaqBtnHidden
+				/>
+			))}
+		</div>
+	);
 
-  return (
-    <section
-      className={cn("help-center", className, {
-        "help-center--rtl": isRTL,
-      })}
-      dir={isRTL ? DIR_RTL : DIR_LTR}
-    >
-      <div className="help-center__wrapper">
-        <h2 className="help-center__title">{t("faq-title")}</h2>
-        <FaqSearchBar setSearchResults={setSearchResults} />
-        {searchResults.length > 0 ? (
-          <HelpCenterBlock
-            faq={searchResults}
-            classNames={["help-center--market"]}
-          />
-        ) : (
-          <>
-            <HelpCenterBlock
-              title={t("faq_quick-title")}
-              titleClassName="help-center__block-title--quick-answer"
-              subtitle={t("faq_quick-subtitle")}
-              faq={FAQ_QUICK_ANSWER}
-              classNames={[
-                "help-center--no-title",
-                "help-center--quick-answer",
-              ]}
-            />
-            <HelpCenterBlock faq={FAQ_ALL} classNames={["help-center--all"]} />
-            <HelpCenterBlock
-              title={
-                <>
-                  <img
-                    src={marketsIcon}
-                    className="help-center__block-icon"
-                    alt=""
-                  />
-                  <span>{t("faq_market-title")}</span>
-                </>
-              }
-              titleClassName="help-center__block-title--market"
-              faq={FAQ_MARKET}
-              classNames={["help-center--market"]}
-            />
-            <HelpCenterBlock
-              title={t("faq_beginners-title")}
-              titleClassName="help-center__block-title--beginners"
-              faq={FAQ_BEGINNERS}
-              classNames={["help-center--no-title", "help-center--beginners"]}
-            />
-          </>
-        )}
-      </div>
-    </section>
-  );
+	return (
+		<section
+			className={cn("help-center", className, {
+				"help-center--rtl": isRTL,
+			})}
+			dir={isRTL ? DIR_RTL : DIR_LTR}
+		>
+			<div className="help-center__wrapper">
+				<h2 className="help-center__title">{t("faq-title")}</h2>
+				<FaqSearchBar setSearchResults={setSearchResults} />
+				{searchResults.length > 0 ? (
+					<HelpCenterBlock
+						faq={searchResults}
+						classNames={["help-center--market"]}
+					/>
+				) : (
+					<>
+						<HelpCenterBlock
+							title={t("faq_quick-title")}
+							titleClassName="help-center__block-title--quick-answer"
+							subtitle={t("faq_quick-subtitle")}
+							faq={FAQ_QUICK_ANSWER}
+							classNames={[
+								"help-center--no-title",
+								"help-center--quick-answer",
+							]}
+						/>
+						<HelpCenterBlock faq={FAQ_ALL} classNames={["help-center--all"]} />
+						<HelpCenterBlock
+							title={
+								<>
+									<img
+										src={marketsIcon}
+										className="help-center__block-icon"
+										alt=""
+									/>
+									<span>{t("faq_market-title")}</span>
+								</>
+							}
+							titleClassName="help-center__block-title--market"
+							faq={FAQ_MARKET}
+							classNames={["help-center--market"]}
+						/>
+						<HelpCenterBlock
+							title={t("faq_beginners-title")}
+							titleClassName="help-center__block-title--beginners"
+							faq={FAQ_BEGINNERS}
+							classNames={["help-center--no-title", "help-center--beginners"]}
+						/>
+					</>
+				)}
+			</div>
+		</section>
+	);
 };
 
 export default HelpCenter;
