06ef7800fb413070e00d7f444cbe29c581ab2c8b
Yiangos <ystroppos@oqtima.com>
2023-01-09 12:40:02 +0200

    initial push to test the chat language

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,73 +1,81 @@
 require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
+	path: `.env.${process.env.NODE_ENV}`,
 });
 
 const languages = require(`${__dirname}/src/locales/language.config`);
-const { processLanguagesForConfig } = require(`${__dirname}/src/locales/processLanguages`);
+const {
+	processLanguagesForConfig,
+} = require(`${__dirname}/src/locales/processLanguages`);
 
 const indexedLocaleData = processLanguagesForConfig(languages.list);
 
 module.exports = {
-  siteMetadata: {
-    title: `website`,
-    siteUrl: `https://www.yourdomain.tld`,
-    indexedLocaleData
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/assets/images/icon.png",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/assets/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "locale",
-        path: `${__dirname}/src/locales/`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        trackingIds: [process.env.GATSBY_GA],
-        gtagConfig: {
-          anonymize_ip: false,
-        },
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: process.env.GATSBY_GOOGLE_TAG_MANAGER,
-        defaultDataLayer: { platform: "gatsby" },
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-i18next",
-      options: {
-        localeJsonSourceName: "locale", // name given to "gatsby-source-filesystem" plugin.
-        languages: languages.list,
-        defaultLanguage: languages.defaultLangKey,
-        fallbackLanguage: languages.defaultLangKey,
-        i18nextOptions: {
-          keySeparator: false,
-          nsSeparator: false
-        },
-      }
-    },
-  ],
+	siteMetadata: {
+		title: `website`,
+		siteUrl: `https://www.yourdomain.tld`,
+		indexedLocaleData,
+	},
+	plugins: [
+		"gatsby-plugin-sass",
+		"gatsby-plugin-image",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/assets/images/icon.png",
+			},
+		},
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/assets/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "locale",
+				path: `${__dirname}/src/locales/`,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				trackingIds: [process.env.GATSBY_GA],
+				gtagConfig: {
+					anonymize_ip: false,
+				},
+			},
+		},
+		{
+			resolve: "gatsby-plugin-google-tagmanager",
+			options: {
+				id: process.env.GATSBY_GOOGLE_TAG_MANAGER,
+				defaultDataLayer: { platform: "gatsby" },
+			},
+		},
+		{
+			resolve: "gatsby-plugin-react-i18next",
+			options: {
+				localeJsonSourceName: "locale", // name given to "gatsby-source-filesystem" plugin.
+				languages: languages.list,
+				defaultLanguage: languages.defaultLangKey,
+				fallbackLanguage: languages.defaultLangKey,
+				i18nextOptions: {
+					keySeparator: false,
+					nsSeparator: false,
+				},
+			},
+		},
+		{
+			resolve: "gatsby-plugin-html-attributes",
+			options: {
+				lang: languages.list,
+			},
+		},
+	],
 };
