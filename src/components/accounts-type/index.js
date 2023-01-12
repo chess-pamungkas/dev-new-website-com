515fcd0837af8ec503b95c6a4c4117f1f3ff8993
Yiangos Stroppos <stroppos.yiangos@gmail.com>
2023-01-12 09:26:41 +0200

    refactored code

--- src/components/accounts-type/index.js
+++ src/components/accounts-type/index.js
@@ -2,41 +2,24 @@ import React from "react";
 import cn from "classnames";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import HighlightedLocalizationText from "../../components/shared/highlighted-localization-text";
-import { ACCOUNT_TYPES } from "../../helpers/accounts-type.config";
+import {
+  ACCOUNT_TYPES,
+  ACCOUNT_TYPES_EU,
+} from "../../helpers/accounts-type.config";
 import AccountType from "./account-type";
 import { stringTransformToKebabCase } from "../../helpers/services/string-service";
 import { useRtlDirection } from "../../helpers/hooks/use-rtl-direction";
 import { DIR_LTR, DIR_RTL } from "../../helpers/constants";
 import { useEntityPostfix } from "../../helpers/use-entity-postfix";
 
-const getAccountTypeWithCysec = (isCySEC) => {
-  if (isCySEC) {
-    return ACCOUNT_TYPES.map((type) => {
-      return {
-        ...type,
-        advantages: type.advantages.map((advantage) => {
-          if (
-            [
-              "accounts-type_account-type1-markets-title",
-              "accounts-type_account-type1-total-symbols-title",
-              "accounts-type_account-type2-markets-title",
-              "accounts-type_account-type2-total-symbols-title",
-            ].includes(advantage.title)
-          ) {
-            return { ...advantage, value: advantage.value + "-eu" };
-          }
-          return advantage;
-        }),
-      };
-    });
-  }
-  return ACCOUNT_TYPES;
-};
+const getAccountTypeWithCysec = (isCySEC) =>
+  isCySEC ? ACCOUNT_TYPES_EU : ACCOUNT_TYPES;
 
 const AccountsType = ({ className }) => {
   const { t } = useTranslation();
   const isRTL = useRtlDirection();
   const { isCySEC } = useEntityPostfix();
+
   return (
     <section
       className={cn("accounts-type", className, {
