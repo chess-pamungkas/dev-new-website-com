272d0be250bddecc15216037d55918a73100dc29
Yiangos <ystroppos@oqtima.com>
2023-01-13 09:51:42 +0200

    removed mt4 ios download and added new text

--- src/components/pages-content/mt4-page-content/index.js
+++ src/components/pages-content/mt4-page-content/index.js
@@ -50,9 +50,6 @@ const Mt4PageContent = () => {
           <Link to={MT4_DOWNLOAD_LINKS.android}>
             {t("mt4_mt-promotion-download-android")}
           </Link>
-          <Link to={MT4_DOWNLOAD_LINKS.ios}>
-            {t("mt4_mt-promotion-download-ios")}
-          </Link>
         </>
       ),
     },
