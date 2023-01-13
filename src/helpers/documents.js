f63c4937be958656b972cd392802d4675fdbed8a
Aliaksandr Baryhin <Aliaksandr.Baryhin@itechart-group.com>
2023-01-13 16:23:48 +0300

    Fixes ow-194. Added Cookie Policy document for the FSA.

--- src/helpers/documents.js
+++ src/helpers/documents.js
@@ -2,6 +2,7 @@ import bestExecutionPolicyFSA from "../assets/documents/fsa/Best_Execution_Polic
 import complaintHandlingPolicyFSA from "../assets/documents/fsa/Complaint_Handling_Policy_V1.pdf";
 import privacyPolicyFSA from "../assets/documents/fsa/Privacy_Policy_V1.pdf";
 import riskDisclosureNoticeFSA from "../assets/documents/fsa/Risk_Disclosure_Notice_V1.pdf";
+import cookiePolicyFSA from "../assets/documents/fsa/Cookie_Policy_V1.pdf";
 
 import privacyPolicy from "../assets/documents/eu/Privacy_Policy-.pdf";
 import clientCategorisationNotice from "../assets/documents/eu/Client_Categorisation_Notice.pdf";
@@ -95,6 +96,10 @@ export const LEGAL_DOCS_FSA = [
   {
     name: "document-risk-disclosure-notice-fsa",
     file: riskDisclosureNoticeFSA,
+  },
+  {
+    name: "document-cookie-policy-fsa",
+    file: cookiePolicyFSA,
   },
 ];
 
