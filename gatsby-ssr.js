806a12e0385615fc23bf591112e6e79ee89a2a99
Yiangos <ystroppos@oqtima.com>
2023-01-13 14:08:02 +0200

    fixed spacing

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,32 +1,32 @@
 const React = require("react");
 
 export const onRenderBody = ({ setPostBodyComponents, setHeadComponents }) => {
-	setPostBodyComponents([
-		<script
-			key="live-chat"
-			defer
-			id="convrs-webchat"
-			src={process.env.GATSBY_CONVRS_LIVECHAT}
-		/>,
-	]);
-	setHeadComponents([
-		<script
-			key="disable-ga"
-			dangerouslySetInnerHTML={{
-				__html: `window['ga-disable-${process.env.GATSBY_GA}'] = true;`,
-			}}
-		/>,
-	]);
+  setPostBodyComponents([
+    <script
+      key="live-chat"
+      defer
+      id="convrs-webchat"
+      src={process.env.GATSBY_CONVRS_LIVECHAT}
+    />,
+  ]);
+  setHeadComponents([
+    <script
+      key="disable-ga"
+      dangerouslySetInnerHTML={{
+        __html: `window['ga-disable-${process.env.GATSBY_GA}'] = true;`,
+      }}
+    />,
+  ]);
 };
 
 export const onPreRenderHTML = ({
-	getHeadComponents,
-	replaceHeadComponents,
+  getHeadComponents,
+  replaceHeadComponents,
 }) => {
-	const headComponents = getHeadComponents();
+  const headComponents = getHeadComponents();
 
-	const orderedComponents = headComponents.sort((item) =>
-		item.key === "disable-ga" ? -1 : 1
-	);
-	replaceHeadComponents(orderedComponents);
+  const orderedComponents = headComponents.sort((item) =>
+    item.key === "disable-ga" ? -1 : 1
+  );
+  replaceHeadComponents(orderedComponents);
 };
--- src/components/pages-content/funding-page-content/index.js
+++ src/components/pages-content/funding-page-content/index.js
@@ -3,10 +3,10 @@ import cn from "classnames";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import TableComponent from "../../shared/table";
 import {
-	COLUMNS_DEPOSIT,
-	COLUMNS_WITHDRAWAL,
-	DATA_DEPOSIT,
-	DATA_WITHDRAWAL,
+  COLUMNS_DEPOSIT,
+  COLUMNS_WITHDRAWAL,
+  DATA_DEPOSIT,
+  DATA_WITHDRAWAL,
 } from "../../../helpers/withdrawal.config";
 import TopMarketPromotion from "../../top-market-promotion";
 import promotion from "../../../assets/images/withdrawal/promotion.svg";
@@ -19,122 +19,122 @@ import { useRtlDirection } from "../../../helpers/hooks/use-rtl-direction";
 import { useEntityPostfix } from "../../../helpers/use-entity-postfix";
 
 const FundingPageContent = () => {
-	const { t } = useTranslation();
-	const isRTL = useRtlDirection();
-	const { isCySEC } = useEntityPostfix();
+  const { t } = useTranslation();
+  const isRTL = useRtlDirection();
+  const { isCySEC } = useEntityPostfix();
 
-	const tabs = [
-		{
-			id: 1,
-			title: t("withdrawal_tabs_title1"),
-			content: (
-				<TableComponent
-					data={DATA_DEPOSIT}
-					columns={COLUMNS_DEPOSIT}
-					className="withdrawal-table"
-				/>
-			),
-		},
-		{
-			id: 2,
-			title: t("withdrawal_tabs_title2"),
-			content: (
-				<TableComponent
-					data={DATA_WITHDRAWAL}
-					columns={COLUMNS_WITHDRAWAL}
-					className={cn("withdrawal-table", "withdrawal-table--wide")}
-				/>
-			),
-		},
-	];
-	return (
-		<>
-			<TopMarketPromotion
-				className={cn("withdrawal-page-promotion", {
-					"split-bg--rtl": isRTL,
-					"withdrawal-page-promotion--rtl": isRTL,
-				})}
-				image={promotion}
-				note={
-					<HighlightedLocalizationText
-						localizationText={t(
-							isCySEC
-								? "withdrawal_top-market-promo-note-EU"
-								: "withdrawal_top-market-promo-note"
-						)}
-						wordsToHighlight={t(
-							isCySEC
-								? "withdrawal_top-market-promo-note-accent-EU"
-								: "withdrawal_top-market-promo-note-accent"
-						)}
-						primaryClassName="highlighted-in-black"
-						accentClassName="highlighted-in-white"
-					/>
-				}
-			>
-				<HighlightedLocalizationText
-					localizationText="withdrawal_top-market-promo-text"
-					wordsToHighlight="withdrawal_top-market-promo-text-accent"
-					primaryClassName="highlighted-in-black"
-					accentClassName="highlighted-in-white"
-				/>
-			</TopMarketPromotion>
-			<TopMarketPromotion
-				className={cn("black-promotion", {
-					"funding-black-promotion--rtl": isRTL,
-				})}
-			>
-				<span className="display-block">
-					<HighlightedLocalizationText
-						localizationText="withdrawal_top-market-promo-text2-1"
-						wordsToHighlight="withdrawal_top-market-promo-text-accent2-1"
-						primaryClassName="highlighted-in-white"
-						accentClassName="highlighted-in-red"
-					/>
-				</span>
-				<span className="display-block">
-					<HighlightedLocalizationText
-						localizationText="withdrawal_top-market-promo-text2-2"
-						wordsToHighlight="withdrawal_top-market-promo-text-accent2-2"
-						primaryClassName="highlighted-in-white"
-						accentClassName="highlighted-in-red"
-					/>
-				</span>
-				<span className="display-block">
-					<HighlightedLocalizationText
-						localizationText="withdrawal_top-market-promo-text2-3"
-						wordsToHighlight="withdrawal_top-market-promo-text-accent2-3"
-						primaryClassName="highlighted-in-white"
-						accentClassName="highlighted-in-red"
-					/>
-				</span>
-			</TopMarketPromotion>
-			<TopMarketLayout className="top-market-layout--withdrawal">
-				<Tabs tabList={tabs} />
-			</TopMarketLayout>
-			<section className={cn("notes-block")}>
-				<p className="notes-block__text">{t("withdrawal_disclaimer1")}</p>
-				<p className="notes-block__text">{t("withdrawal_disclaimer2")}</p>
-				<p className="notes-block__text">{t("withdrawal_disclaimer3")}</p>
-			</section>
-			<TopMarketPromotion
-				className={cn("bottom-promotion", {
-					"bottom-promotion--rtl": isRTL,
-				})}
-				image={icon}
-				btnClassName="button-link--red"
-				btnTitle={t("withdrawal_top-market-promo-btn3")}
-				link={REGISTRATION_LINK}
-			>
-				<HighlightedLocalizationText
-					localizationText="withdrawal_top-market-promo-text3"
-					wordsToHighlight="withdrawal_top-market-promo-text-accent3"
-					primaryClassName="highlighted-in-black"
-					accentClassName="highlighted-in-white"
-				/>
-			</TopMarketPromotion>
-		</>
-	);
+  const tabs = [
+    {
+      id: 1,
+      title: t("withdrawal_tabs_title1"),
+      content: (
+        <TableComponent
+          data={DATA_DEPOSIT}
+          columns={COLUMNS_DEPOSIT}
+          className="withdrawal-table"
+        />
+      ),
+    },
+    {
+      id: 2,
+      title: t("withdrawal_tabs_title2"),
+      content: (
+        <TableComponent
+          data={DATA_WITHDRAWAL}
+          columns={COLUMNS_WITHDRAWAL}
+          className={cn("withdrawal-table", "withdrawal-table--wide")}
+        />
+      ),
+    },
+  ];
+  return (
+    <>
+      <TopMarketPromotion
+        className={cn("withdrawal-page-promotion", {
+          "split-bg--rtl": isRTL,
+          "withdrawal-page-promotion--rtl": isRTL,
+        })}
+        image={promotion}
+        note={
+          <HighlightedLocalizationText
+            localizationText={t(
+              isCySEC
+                ? "withdrawal_top-market-promo-note-EU"
+                : "withdrawal_top-market-promo-note"
+            )}
+            wordsToHighlight={t(
+              isCySEC
+                ? "withdrawal_top-market-promo-note-accent-EU"
+                : "withdrawal_top-market-promo-note-accent"
+            )}
+            primaryClassName="highlighted-in-black"
+            accentClassName="highlighted-in-white"
+          />
+        }
+      >
+        <HighlightedLocalizationText
+          localizationText="withdrawal_top-market-promo-text"
+          wordsToHighlight="withdrawal_top-market-promo-text-accent"
+          primaryClassName="highlighted-in-black"
+          accentClassName="highlighted-in-white"
+        />
+      </TopMarketPromotion>
+      <TopMarketPromotion
+        className={cn("black-promotion", {
+          "funding-black-promotion--rtl": isRTL,
+        })}
+      >
+        <span className="display-block">
+          <HighlightedLocalizationText
+            localizationText="withdrawal_top-market-promo-text2-1"
+            wordsToHighlight="withdrawal_top-market-promo-text-accent2-1"
+            primaryClassName="highlighted-in-white"
+            accentClassName="highlighted-in-red"
+          />
+        </span>
+        <span className="display-block">
+          <HighlightedLocalizationText
+            localizationText="withdrawal_top-market-promo-text2-2"
+            wordsToHighlight="withdrawal_top-market-promo-text-accent2-2"
+            primaryClassName="highlighted-in-white"
+            accentClassName="highlighted-in-red"
+          />
+        </span>
+        <span className="display-block">
+          <HighlightedLocalizationText
+            localizationText="withdrawal_top-market-promo-text2-3"
+            wordsToHighlight="withdrawal_top-market-promo-text-accent2-3"
+            primaryClassName="highlighted-in-white"
+            accentClassName="highlighted-in-red"
+          />
+        </span>
+      </TopMarketPromotion>
+      <TopMarketLayout className="top-market-layout--withdrawal">
+        <Tabs tabList={tabs} />
+      </TopMarketLayout>
+      <section className={cn("notes-block")}>
+        <p className="notes-block__text">{t("withdrawal_disclaimer1")}</p>
+        <p className="notes-block__text">{t("withdrawal_disclaimer2")}</p>
+        <p className="notes-block__text">{t("withdrawal_disclaimer3")}</p>
+      </section>
+      <TopMarketPromotion
+        className={cn("bottom-promotion", {
+          "bottom-promotion--rtl": isRTL,
+        })}
+        image={icon}
+        btnClassName="button-link--red"
+        btnTitle={t("withdrawal_top-market-promo-btn3")}
+        link={REGISTRATION_LINK}
+      >
+        <HighlightedLocalizationText
+          localizationText="withdrawal_top-market-promo-text3"
+          wordsToHighlight="withdrawal_top-market-promo-text-accent3"
+          primaryClassName="highlighted-in-black"
+          accentClassName="highlighted-in-white"
+        />
+      </TopMarketPromotion>
+    </>
+  );
 };
 
 export default FundingPageContent;
