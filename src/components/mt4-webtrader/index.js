6ec32d363559ecc93a3fa451451670d2bdd2fae6
Aliaksandr Baryhin <Aliaksandr.Baryhin@itechart-group.com>
2023-02-23 16:51:44 +0300

    Fixed MT4 webtrader 2

--- src/components/mt4-webtrader/index.js
+++ src/components/mt4-webtrader/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Mt4WebTraderLink = () => {
   return (
     <div style={{}}>
-      <iframe src="mt4-webtrader.html" width="100%" height="650px"></iframe>
+      <iframe src="/mt4-webtrader.html" width="100%" height="650px"></iframe>
     </div>
   );
 };
