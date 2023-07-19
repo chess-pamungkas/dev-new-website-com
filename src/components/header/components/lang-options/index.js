333794c27921470f0060b81d56de1c593cf804e1
Aliaksandr Baryhin <Aliaksandr.Baryhin@itechart-group.com>
2023-07-19 15:16:44 +0300

    Fixes OW-387. Added global footer

--- src/components/header/components/lang-options/index.js
+++ src/components/header/components/lang-options/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import cn from "classnames";
 import { useTranslation } from "gatsby-plugin-react-i18next";
 import {
