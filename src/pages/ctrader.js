f54cc49abdd7373bfe0a23d7f50bed5493388d7f
Aliaksandr Baryhin <Aliaksandr.Baryhin@itechart-group.com>
2023-08-22 15:24:05 +0300

    Fixes OW-409. Hide Ctrader

--- src/pages/ctrader.js
+++ src/pages/ctrader.js
@@ -4,17 +4,31 @@ import { useTranslation } from "gatsby-plugin-react-i18next";
 import "../assets/styles/index.scss";
 import Seo from "../components/shared/seo";
 import CtraderPageContent from "../components/pages-content/ctrader-page-content";
+import comingSoonImage from "../assets/images/system-info/coming-soon.svg";
+import SystemInfoComponent from "../components/shared/system-info";
 
 const CTraderPage = () => {
   const { t } = useTranslation();
 
   return (
+    // Hidden for now
     <>
-      <Seo
+      {/* <Seo
         title={t("page-ctrader-title")}
         description={t("page-ctrader-description")}
+      /> */}
+      <Seo
+        title={t("system-page-coming-soon-title")}
+        fsaRobots={"noindex"}
+        cysecRobots={"noindex"}
       />
-      <CtraderPageContent />
+      {/* <CtraderPageContent /> */}
+      <SystemInfoComponent
+          image={comingSoonImage}
+          title={t("system-page-coming-soon-title")}
+          subTitle={t("system-page-coming-soon-subtitle")}
+          goBackBtnTitle={t("system-page-go-back-btn")}
+        />
     </>
   );
 };
