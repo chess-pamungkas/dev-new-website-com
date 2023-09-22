5b83accde311453d43069673a58c3cfeb801c313
Yiangos <ystroppos@oqtima.com>
2023-09-22 13:42:23 +0300

    OW-428: Added floating button

---
+++ src/components/floating-button/BookmarkButton.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect, useRef } from "react";
+import "../../assets/styles/Bookmark.scss";
+import ButtonLink from "../shared/button-link";
+import { GetRegistrationLink } from "../../helpers/constants";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+
+function Bookmark() {
+  const [isExpanded, setExpanded] = useState(false);
+  const [isVisible, setVisible] = useState(false);
+  const bookmarkRef = useRef(null);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 500) {
+        // Adjust this value as per your first section height
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    };
+
+    const handleClickOutside = (e) => {
+      if (bookmarkRef.current && !bookmarkRef.current.contains(e.target)) {
+        setExpanded(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("click", handleClickOutside);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleBookmarkClick = () => {
+    setExpanded(!isExpanded);
+  };
+
+  const bookmarkClass = `${isExpanded ? "expanded" : ""} ${
+    isVisible ? "" : "hidden"
+  }`;
+
+  return (
+    <div
+      ref={bookmarkRef}
+      className={`bookmark ${bookmarkClass}`}
+      onClick={handleBookmarkClick}
+    >
+      {/* If expanded, show 'Close' otherwise show the bookmark icon */}
+      {isExpanded ? (
+        <ButtonLink
+          link={GetRegistrationLink()}
+          className={"button-link--header header__start"}
+        >
+          {t("button-get-started")}
+        </ButtonLink>
+      ) : (
+        "<"
+      )}
+    </div>
+  );
+}
+
+export default Bookmark;
