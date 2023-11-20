261a60c8bfbca6cf6953fbb24644d1b23da57b48
Aliaksandr Baryhin <Aliaksandr.Baryhin@itechart-group.com>
2023-11-20 11:06:29 +0300

    Fixes OW-457. Remove buttons disabling for banned countries

--- src/components/pages-content/search-page-content/index.js
+++ src/components/pages-content/search-page-content/index.js
@@ -128,7 +128,6 @@ const SearchPageContent = () => {
             <ButtonLink
               link={HOME_PAGE_LINK}
               className="button-link button-link--ghost-red search-page__no-results-btn"
-              ignoreDisabling={true}
             >
               {t("search-go-back-btn")}
             </ButtonLink>
@@ -162,7 +161,6 @@ const SearchPageContent = () => {
                       <ButtonLink
                         link={`${page.url}`}
                         className="search-page__btn button-link--ghost-red"
-                        ignoreDisabling={true}
                       >
                         {t("search-submit-btn")}
                       </ButtonLink>
