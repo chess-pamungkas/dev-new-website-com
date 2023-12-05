09142de26140694428c5e69582d14d5a9d13fb72
Aliaksandr Baryhin <Aliaksandr.Baryhin@itechart-group.com>
2023-12-05 14:26:28 +0300

    Remove hash checks

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import { cloneElement, createElement } from "react";
 import Layout from "./src/components/shared/layout";
-import { isCySEC } from "./src/helpers/entity-resolver";
+
 export const wrapPageElement = ({ element }) => {
   // Don't remove the if statement, it will break everything!!!
   // Workaround to apply localization to layout content, plugin doesn't do this by default
@@ -18,27 +18,4 @@ export const wrapPageElement = ({ element }) => {
   }
 
   return element;
-};
-
-export const onClientEntry = async () => {
-  if (isCySEC() && process.env.NODE_ENV === "production") {
-    try {
-      const response = await fetch("/asset-manifest.json");
-      const manifest = await response.json();
-      const currentHash = manifest.compilationHash;
-
-      const storedHash = window.localStorage.getItem(
-        "gatsby-reload-compilation-hash"
-      );
-      if (storedHash !== currentHash) {
-        window.localStorage.setItem(
-          "gatsby-reload-compilation-hash",
-          currentHash
-        );
-        window.location.reload();
-      }
-    } catch (error) {
-      console.error("Error fetching asset manifest:", error);
-    }
-  }
 };
