b3a85168b12116d342fe0ebefc009bb63f5f6d41
Yiangos Stroppos <stroppos.yiangos@gmail.com>
2023-12-15 12:44:02 +0200

    pushing new fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,6 @@ const { execSync } = require("child_process");
 
 exports.onPostBuild = async () => {
   const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
-  const hashContent = JSON.stringify({ hash: commitHash });
 
-  fs.writeFileSync("public/hash.json", hashContent);
+  fs.writeFileSync("public/hash.json", commitHash);
 };
