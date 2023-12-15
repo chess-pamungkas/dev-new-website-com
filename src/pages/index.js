5f98680a6d920528ef8c3915ba1c928d3bf72877
Yiangos Stroppos <stroppos.yiangos@gmail.com>
2023-12-15 14:23:54 +0200

    pushing new fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,26 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { graphql } from "gatsby";
 import { useTranslationWithVariables } from "../helpers/hooks/use-translation-with-vars";
 import "../assets/styles/index.scss";
+import MainPromotion from "../components/main-promotion";
+import TradingTicker from "../components/trading-ticker";
 import Seo from "../components/shared/seo";
-import VersionSentinel from "../helpers/VersionSentinel";
-import LoadingSpinner from "../components/loading-spinner";
-import { lazified } from "../helpers/lazified";
-
+import PerformanceContent from "../components/pages-content/main-page-content/performance-content";
+import PromotionContent from "../components/pages-content/main-page-content/promotion-content";
 const IndexPage = () => {
-  const MainPromotion = lazified(() => import("../components/main-promotion"));
-  const TradingTicker = lazified(() => import("../components/trading-ticker"));
-  const PerformanceContent = lazified(() =>
-    import("../components/pages-content/main-page-content/performance-content")
-  );
-  const PromotionContent = lazified(() =>
-    import("../components/pages-content/main-page-content/promotion-content")
-  );
-
   const { t } = useTranslationWithVariables();
 
   return (
     <>
-      <Suspense fallback={<LoadingSpinner />}>
-        <VersionSentinel />
-        <Seo
-          title={t("page-main-title")}
-          description={t("page-main-description")}
-        />
-        {/*isShowHero is workaround to hide hero image (e.g. Buffon)  */}
-        <MainPromotion isShowHero />
-        <TradingTicker />
-        <PromotionContent />
-        <PerformanceContent />
-      </Suspense>
+      <Seo
+        title={t("page-main-title")}
+        description={t("page-main-description")}
+      />
+      {/*isShowHero is workaround to hide hero image (e.g. Buffon)  */}
+      <MainPromotion isShowHero />
+      <TradingTicker />
+      <PromotionContent />
+      <PerformanceContent />
     </>
   );
 };
