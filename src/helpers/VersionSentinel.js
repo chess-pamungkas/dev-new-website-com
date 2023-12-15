cff96d9f63421cc6e646c7e12eb24488ec3ac693
Yiangos Stroppos <stroppos.yiangos@gmail.com>
2023-12-15 16:42:48 +0200

    pushing new fix

--- src/helpers/VersionSentinel.js
+++ src/helpers/VersionSentinel.js
@@ -1,36 +1,41 @@
 import { useEffect, useState } from "react";
 import { versionService } from "./versionService";
 import { hasFeature } from "./features";
-
 const VersionSentinel = () => {
   const COMMIT_HASH = process.env.GATSBY_COMMIT_HASH;
-  const [serverHash, setServerHash] = useState(COMMIT_HASH);
+  const isLocal = window.location.hostname === "localhost";
+  const [serverHash, setServerHash] = useState(isLocal ? "local" : COMMIT_HASH);
 
   useEffect(() => {
     const fetchServerHash = async () => {
       try {
-        const hash = await versionService.getVersion();
-        setServerHash(hash);
+        if (!isLocal) {
+          const hash = await versionService.getVersion();
+          setServerHash(hash);
+        }
       } catch (error) {
         console.error("Error fetching version:", error);
       }
     };
 
-    fetchServerHash();
-  }, []);
+    if (!isLocal) {
+      fetchServerHash();
+    }
+  }, [isLocal]);
 
   useEffect(() => {
-    if (
+    const shouldReload =
       hasFeature("versionSentinel") &&
       serverHash !== COMMIT_HASH &&
-      !localStorage.getItem("reloaded")
-    ) {
+      !localStorage.getItem("reloaded");
+
+    if (shouldReload) {
       console.log(
         `Version mismatch detected. Current: ${COMMIT_HASH}, Server: ${serverHash}`
       );
       localStorage.setItem("reloaded", "true");
       setTimeout(() => window.location.reload(), 2000); // Add a delay before reload
-    } else {
+    } else if (!shouldReload && localStorage.getItem("reloaded")) {
       localStorage.removeItem("reloaded"); // Clear the flag if hashes match
     }
   }, [serverHash]);
