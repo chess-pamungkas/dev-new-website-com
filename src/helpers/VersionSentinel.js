3f3c16c989f901d9819f3077b17ec41698691da0
Yiangos Stroppos <stroppos.yiangos@gmail.com>
2023-12-16 08:54:57 +0200

    pushing new fix

--- src/helpers/VersionSentinel.js
+++ src/helpers/VersionSentinel.js
@@ -1,44 +1,50 @@
 import { useEffect, useState } from "react";
 import { versionService } from "./versionService";
 import { hasFeature } from "./features";
-import { isBrowser } from "./services/is-browser";
+import { isBrowser } from "./services/is-browser"; // Ensure this is the correct path
 
 const VersionSentinel = () => {
   const COMMIT_HASH = process.env.GATSBY_COMMIT_HASH;
-  const isLocal = isBrowser() && window.location.hostname === "localhost";
-  const [serverHash, setServerHash] = useState(isLocal ? "local" : COMMIT_HASH);
+  const [serverHash, setServerHash] = useState(COMMIT_HASH);
 
   useEffect(() => {
-    const fetchServerHash = async () => {
-      try {
-        if (!isLocal) {
-          const hash = await versionService.getVersion();
-          setServerHash(hash);
+    if (isBrowser()) {
+      const isLocal = window.location.hostname === "localhost";
+      const fetchServerHash = async () => {
+        try {
+          if (!isLocal) {
+            const hash = await versionService.getVersion();
+            setServerHash(hash);
+          } else {
+            setServerHash("local");
+          }
+        } catch (error) {
+          console.error("Error fetching version:", error);
         }
-      } catch (error) {
-        console.error("Error fetching version:", error);
-      }
-    };
+      };
 
-    if (!isLocal) {
-      fetchServerHash();
+      if (!isLocal) {
+        fetchServerHash();
+      }
     }
-  }, [isLocal]);
+  }, []);
 
   useEffect(() => {
-    const shouldReload =
-      hasFeature("versionSentinel") &&
-      serverHash !== COMMIT_HASH &&
-      !localStorage.getItem("reloaded");
+    if (isBrowser()) {
+      const shouldReload =
+        hasFeature("versionSentinel") &&
+        serverHash !== COMMIT_HASH &&
+        !localStorage.getItem("reloaded");
 
-    if (shouldReload) {
-      console.log(
-        `Version mismatch detected. Current: ${COMMIT_HASH}, Server: ${serverHash}`
-      );
-      localStorage.setItem("reloaded", "true");
-      setTimeout(() => window.location.reload(), 2000); // Add a delay before reload
-    } else if (!shouldReload && localStorage.getItem("reloaded")) {
-      localStorage.removeItem("reloaded"); // Clear the flag if hashes match
+      if (shouldReload) {
+        console.log(
+          `Version mismatch detected. Current: ${COMMIT_HASH}, Server: ${serverHash}`
+        );
+        localStorage.setItem("reloaded", "true");
+        setTimeout(() => window.location.reload(), 2000);
+      } else if (!shouldReload && localStorage.getItem("reloaded")) {
+        localStorage.removeItem("reloaded");
+      }
     }
   }, [serverHash]);
 
