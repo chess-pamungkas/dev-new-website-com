9d59c5739c69d6dc3da1011972e76d0b77ace3b7
Yiangos Stroppos <stroppos.yiangos@gmail.com>
2023-12-18 08:29:39 +0200

    pushing new fix - dynamic loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { graphql } from "gatsby";
 import { useTranslationWithVariables } from "../helpers/hooks/use-translation-with-vars";
 import "../assets/styles/index.scss";
-import MainPromotion from "../components/main-promotion";
-import TradingTicker from "../components/trading-ticker";
 import Seo from "../components/shared/seo";
-import PerformanceContent from "../components/pages-content/main-page-content/performance-content";
-import PromotionContent from "../components/pages-content/main-page-content/promotion-content";
+import { lazified } from "../helpers/lazified";
+import LoadingSpinner from "../components/loading-spinner";
+import VersionSentinel from "../helpers/VersionSentinel";
+
+const MainPromotion = lazified(() => import("../components/main-promotion"));
+const TradingTicker = lazified(() => import("../components/trading-ticker"));
+const PerformanceContent = lazified(() =>
+  import("../components/pages-content/main-page-content/performance-content")
+);
+const PromotionContent = lazified(() =>
+  import("../components/pages-content/main-page-content/promotion-content")
+);
 const IndexPage = () => {
   const { t } = useTranslationWithVariables();
 
   return (
     <>
-      <Seo
-        title={t("page-main-title")}
-        description={t("page-main-description")}
-      />
-      {/*isShowHero is workaround to hide hero image (e.g. Buffon)  */}
-      <MainPromotion isShowHero />
-      <TradingTicker />
-      <PromotionContent />
-      <PerformanceContent />
+      <Suspense fallback={<LoadingSpinner />}>
+        <VersionSentinel />
+        <Seo
+          title={t("page-main-title")}
+          description={t("page-main-description")}
+        />
+        {/*isShowHero is workaround to hide hero image (e.g. Buffon)  */}
+        <MainPromotion isShowHero />
+        <TradingTicker />
+        <PromotionContent />
+        <PerformanceContent />
+      </Suspense>
     </>
   );
 };
