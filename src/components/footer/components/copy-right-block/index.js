76341feb322c8ec24b510f01891f17555bf0a67e
Yiangos Stroppos <stroppos.yiangos@gmail.com>
2024-01-19 14:53:18 +0200

    OW-484: Footer Copyrights FSA and EU

--- src/components/footer/components/copy-right-block/index.js
+++ src/components/footer/components/copy-right-block/index.js
@@ -1,12 +1,22 @@
 import React from "react";
 import cn from "classnames";
-import { getFooterCopyright } from "../../../../helpers/footer.config";
+import {
+  getFooterCompanyName,
+  getFooterCopyright,
+} from "../../../../helpers/footer.config";
 
 const CopyRightBlock = ({ className }) => {
   return (
-    <section className={cn("copy-right-block", className)}>
-      <div className="copy-right-block__text">{getFooterCopyright()}</div>
-    </section>
+    <>
+      <section className={cn("copy-right-block", className)}>
+        <div className="copy-right-block__text">{getFooterCopyright()}</div>
+      </section>
+      <section className={cn("copy-right-block-company", className)}>
+        <div className="copy-right-block-company__text">
+          {getFooterCompanyName()}
+        </div>
+      </section>
+    </>
   );
 };
 
