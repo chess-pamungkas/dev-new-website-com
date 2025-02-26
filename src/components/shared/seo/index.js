4d1e437f9713f04639aab2d58fea45c2ed7d1776
Catur Pamungkas <c.pamungkas@oqtima.com>
2025-02-26 12:19:48 +0700

    fix: Oqtima websites display issue on large screen

--- src/components/shared/seo/index.js
+++ src/components/shared/seo/index.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { Helmet } from "react-helmet";
 import PropTypes from "prop-types";
-import { isCySEC } from "../../../helpers/entity-resolver";
 import LanguageContext from "../../../context/language-context";
 import { useRtlDirection } from "../../../helpers/hooks/use-rtl-direction";
 import { DIR_LTR, DIR_RTL } from "../../../helpers/constants";
@@ -9,16 +8,7 @@ import { DIR_LTR, DIR_RTL } from "../../../helpers/constants";
 const noIndex = Boolean(Number(process.env.GATSBY_NOINDEX));
 const microsoftAds = process.env.GATSBY_MICROSOFT_ADS;
 
-const Seo = ({
-  title,
-  description,
-  fsaTitle,
-  cysecTitle,
-  fsaDescription,
-  cysecDescription,
-  fsaRobots,
-  cysecRobots,
-}) => {
+const Seo = ({ title, description, fsaTitle, fsaDescription, fsaRobots }) => {
   const { selectedLanguage } = useContext(LanguageContext);
   const isRTL = useRtlDirection();
 
@@ -29,19 +19,13 @@ const Seo = ({
         dir: isRTL ? DIR_RTL : DIR_LTR,
       }}
     >
-      <title>{title || (isCySEC ? cysecTitle : fsaTitle)}</title>
-      <meta
-        name="description"
-        content={description || (isCySEC ? cysecDescription : fsaDescription)}
-      />
+      <title>{title || fsaTitle}</title>
+      <meta name="description" content={description || fsaDescription} />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
       />
-      <meta
-        name="robots"
-        content={noIndex ? "noindex" : isCySEC ? cysecRobots : fsaRobots}
-      />
+      <meta name="robots" content={noIndex ? "noindex" : fsaRobots} />
       {microsoftAds && <meta name="msvalidate.01" content={microsoftAds} />}
     </Helmet>
   );
@@ -51,10 +35,8 @@ Seo.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   fsaTitle: PropTypes.string,
-  cysecTitle: PropTypes.string,
   fsaDescription: PropTypes.string,
-  cysecDescription: PropTypes.string,
   fsaRobots: PropTypes.string,
-  cysecRobots: PropTypes.string,
 };
+
 export default Seo;
