01c38cb9a05cf79dbed22e2bdbfd8f8bc2634c3e
Catur Pamungkas <c.pamungkas@oqtima.com>
2025-02-26 11:42:29 +0700

    fix: Oqtima websites display issue on large screen

--- src/components/shared/seo/index.js
+++ src/components/shared/seo/index.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Helmet } from "react-helmet";
 import PropTypes from "prop-types";
+import { isCySEC } from "../../../helpers/entity-resolver";
 import LanguageContext from "../../../context/language-context";
 import { useRtlDirection } from "../../../helpers/hooks/use-rtl-direction";
 import { DIR_LTR, DIR_RTL } from "../../../helpers/constants";
@@ -8,7 +9,16 @@ import { DIR_LTR, DIR_RTL } from "../../../helpers/constants";
 const noIndex = Boolean(Number(process.env.GATSBY_NOINDEX));
 const microsoftAds = process.env.GATSBY_MICROSOFT_ADS;
 
-const Seo = ({ title, description, fsaTitle, fsaDescription, fsaRobots }) => {
+const Seo = ({
+  title,
+  description,
+  fsaTitle,
+  cysecTitle,
+  fsaDescription,
+  cysecDescription,
+  fsaRobots,
+  cysecRobots,
+}) => {
   const { selectedLanguage } = useContext(LanguageContext);
   const isRTL = useRtlDirection();
 
@@ -19,9 +29,19 @@ const Seo = ({ title, description, fsaTitle, fsaDescription, fsaRobots }) => {
         dir: isRTL ? DIR_RTL : DIR_LTR,
       }}
     >
-      <title>{title || fsaTitle}</title>
-      <meta name="description" content={description || fsaDescription} />
-      <meta name="robots" content={noIndex ? "noindex" : fsaRobots} />
+      <title>{title || (isCySEC ? cysecTitle : fsaTitle)}</title>
+      <meta
+        name="description"
+        content={description || (isCySEC ? cysecDescription : fsaDescription)}
+      />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+      />
+      <meta
+        name="robots"
+        content={noIndex ? "noindex" : isCySEC ? cysecRobots : fsaRobots}
+      />
       {microsoftAds && <meta name="msvalidate.01" content={microsoftAds} />}
     </Helmet>
   );
@@ -31,8 +51,10 @@ Seo.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   fsaTitle: PropTypes.string,
+  cysecTitle: PropTypes.string,
   fsaDescription: PropTypes.string,
+  cysecDescription: PropTypes.string,
   fsaRobots: PropTypes.string,
+  cysecRobots: PropTypes.string,
 };
-
 export default Seo;
